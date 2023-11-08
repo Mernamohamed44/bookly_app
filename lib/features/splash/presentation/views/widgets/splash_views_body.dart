237d1495c530299516e85abe9b21de2e4f7ccba0
Merna elseady <78154455+Mernamohamed44@users.noreply.github.com>
2023-11-08 11:44:56 +0200

    splash_screen

---
+++ lib/features/splash/presentation/views/widgets/splash_views_body.dart
@@ -0,0 +1,55 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presenation/views/home_views.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashViewsBody extends StatefulWidget {
+  const SplashViewsBody({super.key});
+
+  @override
+  State<SplashViewsBody> createState() => _SplashViewsBodyState();
+}
+
+class _SplashViewsBodyState extends State<SplashViewsBody> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    initSlidingText();
+    moveToHome();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    animationController;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children:  [
+        const Image(image: AssetImage(AssetsData.logo)),
+        const SizedBox(height: 4,),
+        SlidingText(slidingAnimation: slidingAnimation)
+      ],
+    );
+  }
+  void initSlidingText(){
+    animationController=AnimationController(vsync:this,duration: const Duration(seconds: 1) );
+    slidingAnimation=Tween<Offset>(
+        begin: const Offset(0,12),
+        end:  Offset.zero
+    ).animate(animationController);
+    animationController.forward();
+  }
+  void moveToHome(){
+    Future.delayed(const Duration(seconds: 3),() => Get.to(HomeViews(),transition:Transition.fade,duration: kTranactionDuration ),);
+
+  }
+}
